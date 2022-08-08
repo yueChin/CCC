149a7a9a59d3e947b9b422e68c86f643dd3e32b5
yueChin <transuni@foxmail.com>
2022-08-08 17:45:21 +0800

    碰撞引入

---
+++ 3C/Assets/CCCollision/Collision/Structs/CollisionInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace CapsuleCharacterCollisionDetection
+{
+	public struct CollisionInfo
+	{
+		public Vector3 safeMoveDirection;
+		public Vector3 velocity;
+		public bool hasCollided;
+		public bool hasFailed;
+		public int attempts;
+	}
+}
