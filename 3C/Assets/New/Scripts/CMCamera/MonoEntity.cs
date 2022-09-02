cca8db2d900560f874124eb95357f0545ed196c6
yueChin <transuni@foxmail.com>
2022-09-03 01:12:37 +0800

    Entity 应该记录自身的buff

---
+++ 3C/Assets/New/Scripts/CMCamera/MonoEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoEntity : MonoBehaviour
+{
+    public readonly Dictionary<int,Buff> OwnBuffDict = new Dictionary<int,Buff>();
+
+    protected virtual void Awake()
+    {
+        
+    }
+
+    public bool HasBuff(int id)
+    {
+        return OwnBuffDict.ContainsKey(id);
+    }
+    
+
+    public void AddBuff(Buff buff)
+    {
+        // if(buff.BuffId == 1)
+        //     Debug.LogError("增加");
+        OwnBuffDict.TryAdd(buff.BuffId, buff);
+    }
+
+    public bool RemoveBuff(int id)
+    {
+        // if(id == 1)
+        //     Debug.LogError("移除");
+        if (OwnBuffDict.ContainsKey(id))
+        {
+            OwnBuffDict.Remove(id);
+            return true;
+        }
+        return false;
+    }
+}
